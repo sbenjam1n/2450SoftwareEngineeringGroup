aef08bff9b34213f26d2e70668e867c3ea79e623
taylorbasso <taylor008@gmail.com>
2016-07-14 19:22:41 -0600

    Delete search.js

--- search.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-function searchGoogle() {
-    var searchString = document.getElementById('searchString').value;
-    document.getElementById('googles').action = "https://www.google.com/?gws_rd=ssl#q=" + searchString; //Will set it
-  }
